8f6aa11ca026f465348489ae00a7740e3586057e
BrayanBaez <124947178+BrayanBaez@users.noreply.github.com>
2023-03-06 21:27:00 -0600

    Practica 2

---
+++ String_comillas.py
@@ -0,0 +1,12 @@
+#Practica 2
+#Escribe dos variables con strings. Uno con comillas simples '' y otro con comillas dobles"".
+#Almacena literalmente esta frase en una variable e imprímela:
+#"print()" se utiliza para imprimir valores en la consola.
+
+tex1 = "Acabo de escribir entre comillas dobles" #Crear variable string usando ""
+
+text2 = 'Ahora lo hago en comillas simples'      #Crear variable string usando ''
+
+text3 = '"print()" se utiliza para imprimir valores en la consola '  #Crear variable y agregarle el texto mencionado en la practica
+
+print(text3) #Imprimir el texto
