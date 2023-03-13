bc2f71eb5cf210d62deb0c899ef8190614290fcb
BrayanBaez <124947178+BrayanBaez@users.noreply.github.com>
2023-03-12 23:16:36 -0600

    Practica 30

---
+++ Diccionarios_3_Del_Len.py
@@ -0,0 +1,17 @@
+""" Diccionario 3"""
+
+teclado1 = {
+	'Categoría': 'Teclados',
+	'Modelo': 'HyperX Alloy FPS Pro',
+	'Precio': '89,99'
+}
+
+teclado2 = {
+	'Categoría': 'Teclados',
+	'Modelo': 'Corsair K55 RGB',
+	'Precio': '59,99'
+}
+
+del teclado1, teclado2['Categoría'], teclado2['Modelo']#eliminar teclado 1 y llaves del teclado 2
+
+print(teclado2)
