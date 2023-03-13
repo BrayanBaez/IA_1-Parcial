ade6a4b3b22ea5c71f6bf62e255c124bb3c99293
BrayanBaez <124947178+BrayanBaez@users.noreply.github.com>
2023-03-12 22:12:19 -0600

    Practica 28

---
+++ Diccionarios_1.py
@@ -0,0 +1,16 @@
+""" Diccionario 1"""
+
+teclado1 = { # Primera key
+	'Categoría': 'Teclados',
+	'Modelo': 'HyperX Alloy FPS Pro',
+	'Precio': '89,99'
+}
+
+teclado2 = { # Segunda key
+	'Categoría': 'Teclados',
+	'Modelo': 'Corsair K55 RGB',
+	'Precio': '59,99'
+}
+
+
+print("El modelo del teclado 2 es:",teclado2['Modelo'], "con un precio de:$", teclado2['Precio'] )
