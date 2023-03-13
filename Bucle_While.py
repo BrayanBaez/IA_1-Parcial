a4a255ca18657d1493b8775c294962bda8c27a54
BrayanBaez <124947178+BrayanBaez@users.noreply.github.com>
2023-03-12 20:49:41 -0600

    Practica 24

---
+++ Bucle_While.py
@@ -0,0 +1,24 @@
+
+"""Condicional WHILE """
+
+x = 0
+x2 = 0
+x3 = 10
+print("Primer ejercicio\n")
+while x < 15: #bucle while
+       
+    print("El valor de x es:", x, "Aun no llega al valor 15\nse le aumentaran 5 extra") #Print para ver cuanto vale x y a que debe de llegar
+    x += 5
+
+  
+print("\n\nSegundo ejercicio\n")
+while x2 > -100: #bucle while
+       
+    print("El valor de x2 es:", x, "Aun no llega al valor -100 \nse le aumentaran 5 extra") #Print para ver cuanto vale x2 y a que debe de llegar
+    x2 -= 20
+
+print("\n\ntercer ejercicio\n")
+while x3 > 0: #bucle while
+       
+    print("El valor de x es:", x3, "Aun no llega al valor 0\nse le restara 1") #Print para ver cuanto vale x y a que debe de llegar
+    x3 -= 1
