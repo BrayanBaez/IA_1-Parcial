c688b211999e07a60663c84a015ac3cc487c2892
BrayanBaez <124947178+BrayanBaez@users.noreply.github.com>
2023-03-12 19:59:39 -0600

    Practica 21

---
+++ Condicionales_If_Else.py
@@ -0,0 +1,13 @@
+"""Condicional If Else"""
+#programa viejo
+"""color = "rojo
+else color == rojo
+Print El color es rojo.
+if color != rojo
+Print El color no es rojo."""
+
+if color == "rojo" : 
+print("El color no es rojo")
+
+else :
+print("El color no es rojo.") 
